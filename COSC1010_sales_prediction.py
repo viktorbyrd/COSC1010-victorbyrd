ded0a92d81b2ab1e8ffc5f26ef5704f1863a535b
viktorbyrd <victorbyrd@westernwyoming.edu>
2025-09-07 16:06:12 -0600

    Add files via upload
    This is the sales prediction

---
+++ COSC1010_sales_prediction.py
@@ -0,0 +1,15 @@
+#
+# victor byrd
+# 9-7-2025
+# Sales Prediction Programming Project
+# COSC 1010
+#
+
+# Variables to hold the sales total and the profit
+
+# Get the amount of projected sales.
+Total_sales = float(input('Enter the projected sales'))
+# Calculate the projected profit.
+profit = Total_sales * 0.23
+# Print the projected profit.
+print('The profit is $',format(profit,',.2f'))
